bfec2e007e2404f3fa56a4f6427ddb58782825f7
Pepel <lera.kalashnikova2018@gmail.com>
2024-09-03 18:50:28 +0300

    added task 2

--- js/task-2.js
+++ js/task-2.js
@@ -1,6 +1,10 @@
 function makeArray(firstArray, secondArray, maxLength) {
-  const result = firstArray.concat(secondArray);
-  return result;
+  const newArray = firstArray.concat(secondArray);
+  if (newArray.length > maxLength) {
+    return newArray.slice(0, maxLength);
+  } else {
+    return newArray;
+  }
 }
 
 console.log(makeArray(['Mango', 'Poly'], ['Ajax', 'Chelsea'], 3)); // ["Mango", "Poly", "Ajax"]
